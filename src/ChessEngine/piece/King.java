d69779f8ca2f595ee2229c1211d259fbaf32e2f6
congzung <congdung100104@gmail.com>
2024-11-26 18:33:30 +0700

    Fix is checked by queen

--- src/ChessEngine/piece/King.java
+++ src/ChessEngine/piece/King.java
@@ -35,7 +35,7 @@ public class King extends Piece {
 	@Override
 	public ArrayList<Move> calculateLegalMoves(Gameplay game) {
 		Board board = game.board;
-
+		System.out.println("King position: " + this.row + this.col);
 		ArrayList<Move> legalMoves = new ArrayList<>();
 		final Tile tileFrom = board.tiles[this.row][this.col];
 		Tile tiles[][] = board.tiles;
@@ -205,7 +205,6 @@ public class King extends Piece {
 						colBetween += direction[1];
 
 						if (rowBetween == piece.row) {
-							System.out.println("checked by queen");
 							return true;
 						}
 
@@ -216,16 +215,16 @@ public class King extends Piece {
 				}
 
 				// By diagonal
-				if (deltaRow == deltaCol || deltaCol == -deltaCol) {
+				if (deltaRow == deltaCol || deltaRow == -deltaCol) {
+					// Check if there is a piece between them
 					int absDelta = (deltaRow == 0) ? abs(deltaCol) : abs(deltaRow);
 					int[] direction = { deltaRow / absDelta, deltaCol / absDelta };
+
 					int rowBetween = this.row, colBetween = this.col;
 					while (true) {
 						rowBetween += direction[0];
 						colBetween += direction[1];
-
 						if (rowBetween == piece.row) {
-							System.out.println("checked by queen");
 							return true;
 						}
 
